a6a0d75772d8fbbb7ef37ef3c7724ba7b047a1e3
alienat3d <72837855+alienat3d@users.noreply.github.com>
2021-03-24 00:45:34 +0300

    23.3

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
-$(function(){
+$(function () {
+  // *jquery 'slick slider'
+  $('.slider__inner').slick({
+    arrows: false,
+    dots: true,
+  });
 
+  $('.works__inner').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:
+      '<button class="slick-arrow slick-prev"><img src="images/slider/slider-left-arrow.svg"></button>',
+    nextArrow:
+      '<button class="slick-arrow slick-next"><img src="images/slider/slider-right-arrow.svg"></button>',
+  });
 
-    $('.slider__inner').slick({
-        arrows: false,
-        dots: true,
-    });
+  // *burger menu -> change state on click
+  $('.header__menu-btn').on('click', function () {
+    $('.header__menu > ul').slideToggle();
+  });
 
-    $('.works__inner').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/slider-left-arrow.svg"></button>',
-        nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/slider-right-arrow.svg"></button>'
-    });
-
+  // '(this)' using to force JS working only with 1 element, that been clicked on / childen' allows to get inside into the nesting
+  $('.header__drop-down.drop-down').on('click', function () {
+    $(this).children('.drop-down__list').slideToggle('active');
+  });
 });
