5de1e21dd45ffe7aa9fed85830d586ddc14b30ec
alienat3d <72837855+alienat3d@users.noreply.github.com>
2021-03-26 09:39:40 +0300

    26.3

--- js/main.js
+++ js/main.js
@@ -1,26 +1,47 @@
 $(function () {
   // *jquery 'slick slider'
-  $('.slider__inner').slick({
+  $(".slider__inner").slick({
     arrows: false,
     dots: true,
   });
 
-  $('.works__inner').slick({
+  $(".works__inner").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     prevArrow:
       '<button class="slick-arrow slick-prev"><img src="images/slider/slider-left-arrow.svg"></button>',
     nextArrow:
       '<button class="slick-arrow slick-next"><img src="images/slider/slider-right-arrow.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 911,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 681,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 455,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 
   // *burger menu -> change state on click
-  $('.header__menu-btn').on('click', function () {
-    $('.header__menu > ul').slideToggle();
+  $(".header__menu-btn").on("click", function () {
+    $(".header__menu > ul").slideToggle();
   });
 
   // '(this)' using to force JS working only with 1 element, that been clicked on / childen' allows to get inside into the nesting
-  $('.header__drop-down.drop-down').on('click', function () {
-    $(this).children('.drop-down__list').slideToggle('active');
+  $(".header__drop-down.drop-down").on("click", function () {
+    $(this).children(".drop-down__list").toggleClass("active");
+    $(this).children(".drop-down__link").toggleClass("active");
   });
 });
